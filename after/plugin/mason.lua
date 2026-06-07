6c76fcf8d76b6033ad015fe415c4a287528a0912
Saran Movva <saranmovva@gmail.com>
2026-06-06 22:26:52 -0500

    handle lsp deprecations

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -12,24 +12,41 @@ require("mason-lspconfig").setup {
 	}
 }
 
-local lspconfig = require('lspconfig')
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-lspconfig.cssls.setup { capabilities = lsp_capabilities }
-lspconfig.csharp_ls.setup { capabilities = lsp_capabilities }
-lspconfig.lua_ls.setup { 
-		capabilities = lsp_capabilities, 
-		settings = {
-				Lua = {
-						diagnostics = {
-								globals = { 'vim', 'use', 'lazy' },
-						},
-				},
-		},
-}
-lspconfig.ts_ls.setup { capabilities = lsp_capabilities }
-lspconfig.tailwindcss.setup { capabilities = lsp_capabilities }
-lspconfig.gopls.setup { capabilities = lsp_capabilities }
+-- CSS
+vim.lsp.config('cssls', { capabilities = lsp_capabilities })
+vim.lsp.enable('cssls')
+
+-- C#
+vim.lsp.config('csharp_ls', { capabilities = lsp_capabilities })
+vim.lsp.enable('csharp_ls')
+
+-- Lua
+vim.lsp.config('lua_ls', {
+    capabilities = lsp_capabilities,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim', 'use', 'lazy' },
+            },
+        },
+    },
+})
+vim.lsp.enable('lua_ls')
+
+-- TypeScript / JavaScript
+vim.lsp.config('ts_ls', { capabilities = lsp_capabilities })
+vim.lsp.enable('ts_ls')
+
+-- TailwindCSS
+vim.lsp.config('tailwindcss', { capabilities = lsp_capabilities })
+vim.lsp.enable('tailwindcss')
+
+-- Go
+vim.lsp.config('gopls', { capabilities = lsp_capabilities })
+vim.lsp.enable('gopls')
+
 --[[
 require('mason-null-ls').setup {
 	ensure_installed = {
